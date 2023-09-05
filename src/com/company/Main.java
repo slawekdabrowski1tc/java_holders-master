74b685f61d5d74110ab9b438e6144462a0691c40
slawek <slawek@github.local>
2023-09-05 11:13:38 +0200

    addded more lines

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        GenericHolder obj = new GenericHolder(123);
-        obj.setObj("123 + 123 = 246");
-        System.out.println(obj.getObj());
+        GenericHolder genericHolder = new GenericHolder(123);
+        genericHolder.setObj("123 + 123 = 246");
+        System.out.println(genericHolder.getObj());
+        System.out.println(genericHolder.getObj().getClass().getSimpleName());
+        genericHolder.setObj(333);
+        System.out.println(genericHolder.getObj());
+
 
     }
 }
